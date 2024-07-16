c96a95fa559ada2991791249759f9905156ddb84
Nesha-macherla <nesha.macherla@gmail.com>
2024-07-16 12:05:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+# Upgrade pip
 pip install --upgrade pip
-pip install bs4 beautifulsoup4
+
+# Install beautifulsoup4
+pip install beautifulsoup4==4.12.2
+
+# Install other packages one by one
+pip install torch==2.0.1
+pip install sentence-transformers==2.2.2
+pip install transformers==4.30.2
+
+# Install remaining requirements
 pip install -r requirements.txt
